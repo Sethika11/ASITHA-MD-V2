040607ce6dac474a956183436760c1d828027e9a
Sethika11 <garusanjeewani@gmail.com>
2024-11-25 19:08:38 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=2uIFADzZ#67zIEQmGPHakq1PT6-jLrdRVLdKQRZxqDREX21LKK_0' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
-ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*💞Hello , I am alive now!!*     > BOT GRUOP LINK 👇 https://chat.whatsapp.com/EA8scSIjQn1LRkmEJKkPwQ" : process.env.ALIVE_MSG,
+ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "> *💞Hello , I am alive now!! ||| BOT GRUOP LINK 👇 https://chat.whatsapp.com/EA8scSIjQn1LRkmEJKkPwQ JOIN NOW 🔥❤️*" : process.env.ALIVE_MSG,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
 AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "false" : process.env.AUTO_VOICE,
